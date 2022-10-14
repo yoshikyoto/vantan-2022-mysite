86d7dbd4fd179ee5b3887873184debcd7fc08803
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2022-10-14 10:45:35 +0900

    テンプレートを追加

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
+from django.shortcuts import render
 from django.http import HttpResponse
  
 def index(request):
-    return HttpResponse("Hello, world.")
+    return render(request, "blog/index.html")
 
 def detail(request):
     return HttpResponse("detail page")
