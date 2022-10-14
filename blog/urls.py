fcbc5132a690c576f7db797d56ba0313cf5dcc77
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2022-10-14 10:13:49 +0900

    indexとdetailを追加

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+# blog ディレクトリの中の views.py を import
+from blog import views
+
+urlpatterns = [
+    # views.py の index() 関数を呼び出す
+    path('', views.index, name='index'),
+    path('detail', views.detail, name='detail'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+ 
+def index(request):
+    return HttpResponse("Hello, world.")
 
-# Create your views here.
+def detail(request):
+    return HttpResponse("detail page")
